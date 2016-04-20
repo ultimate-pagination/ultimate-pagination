ad37ad5b5ea04739c847e28f0856c014f8599805
Dmytro Yarmak <dmytroy@backbase.com>
2016-04-20 21:29:28 +0300

    Improve alghoritm to always show the same numbers of items in pagination

--- src/ultimate-pagination.js
+++ src/ultimate-pagination.js
@@ -3,22 +3,28 @@ import {createRange} from './ultimate-pagination-utils';
 export function getPaginationModel(currentPage, totalPages) {
   let paginationModel = [];
 
+  // Calculate group of central pages
+  let mainPagesStart = Math.max(currentPage - 1, 2) - Math.max(0, currentPage + 2 - totalPages);
+  let mainPagesEnd = Math.min(currentPage + 1, totalPages - 1) + Math.max(0, 3 - currentPage);
+  let mainPages = createRange(mainPagesStart,  mainPagesEnd);
+
   // Always add the first page
   paginationModel.push(1);
 
-  // Show '...' between the first page and main pages group if needed
-  if(currentPage > 3) {
+  // Show '...' or second page between the last page and main pages group if needed
+  if (mainPagesStart === 3) {
+    paginationModel.push(2);
+  } else if (mainPagesStart > 3) {
     paginationModel.push('...');
   }
 
   // Add pages +/- from the current page
-  let mainPagesStart = Math.max(currentPage - 1, 2);
-  let mainPagesEnd = Math.min(currentPage + 1, totalPages - 1);
-  let mainPages = createRange(mainPagesStart,  mainPagesEnd);
   paginationModel.push(...mainPages);
 
-  // Show '...' between main pages group and the last page if needed
-  if(currentPage < totalPages - 2) {
+  // Show '...' or penult page between main pages group and the last page if needed
+  if (mainPagesEnd === totalPages - 2) {
+    paginationModel.push(totalPages - 1);
+  } else if(mainPagesEnd < totalPages - 2) {
     paginationModel.push('...');
   }
 
--- test/ultimate-pagination-test.js
+++ test/ultimate-pagination-test.js
@@ -1,20 +1,20 @@
 import {getPaginationModel} from '../src/ultimate-pagination';
 
 describe('getPaginationModel', function() {
-  it('should return [1, 2, "...", 10] for 1 of 10', function(){
-    expect(getPaginationModel(1, 10)).toEqual([1, 2, '...', 10]);
+  it('should return [1, 2, 3, 4, "...", 10] for 1 of 10', function(){
+    expect(getPaginationModel(1, 10)).toEqual([1, 2, 3, 4, '...', 10]);
   });
 
-  it('should return [1, 2, 3, "...", 10] for 2 of 10', function(){
-    expect(getPaginationModel(2, 10)).toEqual([1, 2, 3, '...', 10]);
+  it('should return [1, 2, 3, 4, "...", 10] for 2 of 10', function(){
+    expect(getPaginationModel(2, 10)).toEqual([1, 2, 3, 4, '...', 10]);
   });
 
   it('should return [1, 2, 3, 4, "...", 10] for 3 of 10', function(){
     expect(getPaginationModel(3, 10)).toEqual([1, 2, 3, 4, '...', 10]);
   });
 
-  it('should return [1, "...", 3, 4, 5, "...", 10] for 4 of 10', function(){
-    expect(getPaginationModel(4, 10)).toEqual([1, '...', 3, 4, 5, '...', 10]);
+  it('should return [1, 2, 3, 4, 5, "...", 10] for 4 of 10', function(){
+    expect(getPaginationModel(4, 10)).toEqual([1, 2, 3, 4, 5, '...', 10]);
   });
 
   it('should return [1, "...", 4, 5, 6, "...", 10] for 5 of 10', function(){
@@ -25,19 +25,19 @@ describe('getPaginationModel', function() {
     expect(getPaginationModel(6, 10)).toEqual([1, '...', 5, 6, 7, '...', 10]);
   });
 
-  it('should return [1, "...", 6, 7, 8, "...", 10] for 7 of 10', function(){
-    expect(getPaginationModel(7, 10)).toEqual([1, '...', 6, 7, 8, '...', 10]);
+  it('should return [1, "...", 6, 7, 8, 9, 10] for 7 of 10', function(){
+    expect(getPaginationModel(7, 10)).toEqual([1, '...', 6, 7, 8, 9, 10]);
   });
 
   it('should return [1, "...", 7, 8, 9, 10] for 8 of 10', function(){
     expect(getPaginationModel(8, 10)).toEqual([1, '...', 7, 8, 9, 10]);
   });
 
-  it('should return [1, "...", 8, 9, 10] for 9 of 10', function(){
-    expect(getPaginationModel(9, 10)).toEqual([1, '...', 8, 9, 10]);
+  it('should return [1, "...", 7, 8, 9, 10] for 9 of 10', function(){
+    expect(getPaginationModel(9, 10)).toEqual([1, '...', 7, 8, 9, 10]);
   });
 
-  it('should return [1, "...", 9, 10] for 10 of 10', function(){
-    expect(getPaginationModel(10, 10)).toEqual([1, '...', 9, 10]);
+  it('should return [1, "...", 7, 8, 9, 10] for 10 of 10', function(){
+    expect(getPaginationModel(10, 10)).toEqual([1, '...', 7, 8, 9, 10]);
   });
 });
