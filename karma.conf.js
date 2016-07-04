177d31527e64119005849388436330b2da68c055
Dmytro Yarmak <dmytroy@backbase.com>
2016-07-04 15:58:05 +0300

    Improve karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,76 +1,29 @@
-// Karma configuration
-// Generated on Sat Apr 16 2016 23:02:54 GMT+0300 (EEST)
 var webpackConfig = require('./webpack.config.js');
 
 module.exports = function(config) {
   config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
-
-
-    // list of files / patterns to load in the browser
     files: [
       'test/*-test.ts'
     ],
-
-
-    // list of files to exclude
     exclude: [
     ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
       'test/*-test.ts': ['webpack']
     },
-
     webpack: {
       resolve: webpackConfig.resolve,
       module: webpackConfig.module
     },
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    webpackMiddleware: {noInfo: true},
     reporters: ['progress'],
-
-
-    // web server port
     port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    autoWatch: false,
     browsers: ['PhantomJS'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
+    singleRun: true,
     concurrency: Infinity
   })
 }
