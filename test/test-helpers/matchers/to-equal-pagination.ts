1951423095c8f596a79c67e2806e3a9d612ff67d
Dmytro Yarmak <dmytroy@backbase.com>
2017-02-19 20:05:01 +0200

    test(matchers): show nice message when toEqualPagination failed

--- test/test-helpers/matchers/to-equal-pagination.ts
+++ test/test-helpers/matchers/to-equal-pagination.ts
@@ -51,17 +51,55 @@ function parseExpectedPage(expectedPage: string): PaginationModelItem {
     return {key, type, value, isActive};
 }
 
+function paginationModelItemToSymbolPart(item: PaginationModelItem) {
+        switch (item.type) {
+            case ITEM_TYPES.FIRST_PAGE_LINK: return '<<';
+            case ITEM_TYPES.PREVIOUS_PAGE_LINK: return '<';
+            case ITEM_TYPES.NEXT_PAGE_LINK: return '>';
+            case ITEM_TYPES.LAST_PAGE_LINK: return '>>';
+            case ITEM_TYPES.ELLIPSIS: return (item.key === -1 ? '<...' : '...>');
+        }
+}
+
+function formatPaginationModelToASCII(paginationModel: PaginationModel) {
+    return formatArray(paginationModel.map(item => {
+        const symbolPart = paginationModelItemToSymbolPart(item);
+        return [
+            item.isActive ? '*' : '',
+            item.value,
+            symbolPart ? `(${symbolPart})` : ''
+        ].join('');
+    }));
+}
+
+function formatArray(arr: any[]) {
+    return '[' + arr.join(', ') + ']';
+}
+
+function generateMessage(actual: any[], expected: any[]) {
+    try {
+        return `Expected pagination \n  ${formatPaginationModelToASCII(actual)} \nto equal pagination \n  ${formatArray(expected)}`;
+    } catch (e) {
+        // Fallback to default message on exception
+        return '';
+    }
+}
+
 export const toEqualPagination: jasmine.CustomMatcherFactory = function(util, customEqualityTester) {
     return {
         compare(actual: PaginationModel, expected: string[]) {
             if (actual.length !== expected.length) {
-                return {pass: false};
+                return {
+                    pass: false,
+                    message: generateMessage(actual, expected)
+                };
             } else {
                 return {
                     pass: actual.every((actualItem, i) => {
                         const expectedItem = parseExpectedPage(expected[i]);
                         return util.equals(actualItem, expectedItem);
-                    })
+                    }),
+                    message: generateMessage(actual, expected)
                 };
             }
         }
